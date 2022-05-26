ed66faa4420070efb2b596c7797c6dc2b3929513
Abu Bakkar Siddiq <abu.bakkar@sebpo.com>
2022-05-26 18:22:33 +0600

    tips 1,2

---
+++ README.md
@@ -0,0 +1,19 @@
+#tips1
+
+1.  Remove javaScript falsy valuse
+2.  Convert any values to boolen
+3.  Resize any array to an desire size
+4.  Flatten multi dimentional array
+5.  short conditionals
+6.  replace a word in a sentence or a paragraph
+
+#tips2
+
+1.  swapping values
+2.  copy to clipboard
+3.  destructuring aliases
+4.  get value as data type
+5.  remove duplicated from array
+6.  compare 2 arrays by values
+7.  shuflling array elements
+8.  comma operator
--- tips2.js
+++ tips2.js
@@ -1,35 +1,80 @@
 /**
- * 1.
+ * 1. swapping values
  */
+let arr = [1, 2, 3, 4, 5, 6];
+//array destructuring
+
+[arr[0], arr[5]] = [arr[5], arr[0]];
+console.log(arr);
+//math
+let a = 5;
+let b = 10;
+// [a, b] = [b, a];
+b = a + (a = b) - b;
+console.log(a, b);
+/**
+ * 2. copy to clipboard
+ */
+function copyToClipBoard(str) {
+  const element = document.createElement("textarea");
+  element.value = str;
+  document.body.appendChild(element);
+  element.select();
+  document.execCommand("copy");
+  document.body.removeChild(element);
+}
+function handleClick() {
+  const text = document.querySelector("#text");
+  copyToClipBoard(text.innerHTML);
+}
+/**
+ * 3. destructuring aliases
+ */
+const language = {
+  name: "react",
+  founder: "Jordan Walke",
+};
+
+const { name: languageName, founder: founderName } = language;
+
+console.log(languageName, founderName);
 
 /**
- * 2.
+ * 4. get value as data type
  */
+// const value = document.querySelector("#value").valueAsNumber;
+// console.log(value);
 
 /**
- * 3.
+ * 5. remove duplicated from array
  */
-
+let arr5 = [1, 2, 3, 4, 1, 3, 4, 6, 7, 8];
+console.log([...new Set(arr5)]);
 /**
- * 4.
+ * 6. compare 2 arrays by values
  */
-
+const hasSameElement = (a, b) => {
+  return (
+    a.length === b.length &&
+    a.every((v, i) => {
+      return v === b[i];
+    })
+  );
+};
+let mathcStatus = hasSameElement([1, 22, 3, 4, 5], [1, 2, 3, 4, 5]);
+console.log(mathcStatus);
 /**
- * 5.
+ * 7. shuflling array elements
  */
-
+const numbers = [1, 2, 3, 4, 5, 6];
+let shuffle = numbers.sort(() => Math.random() - 0.5);
+console.log(shuffle);
 /**
- * 6.
+ * 8. comma operator
  */
-
-/**
- * 7.
- */
-
-/**
- * 8.
- */
-
+let x = 1;
+x = (x++, x);
+console.log(x);
 /**
  * 9.
  */
